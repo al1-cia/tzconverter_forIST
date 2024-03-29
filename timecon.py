c5bf422630e86d038b085e5bb2cfdb9ed1edafde
Alicia <alicia.gonsalves_03@outlook.com>
2024-03-29 22:30:15 +0530

    commit

--- timecon.py
+++ timecon.py
@@ -10,7 +10,6 @@ def index():
 @app.route('/convert', methods=['POST'])
 def convert():
     timezones = {
-        "AEST": [4,30],
         "BST": [-4, -30],
         "CST": [-10, -30],
         "PT": [-12, -30],
