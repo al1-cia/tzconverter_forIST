5edcac47d52173b25836a96247320c391cfcbf7b
Alicia <alicia.gonsalves_03@outlook.com>
2024-04-03 11:33:04 +0530

    commit dst changes

--- timecon.py
+++ timecon.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request
 import numpy as np
+from datetime import datetime, timedelta
+import calendar
 
 app = Flask(__name__,static_url_path='/static', static_folder='static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+def is_last_sunday(date):
+    last_day_of_month = calendar.monthrange(date.year, date.month)[1]
+    for day in range(last_day_of_month, 0, -1):
+        if calendar.weekday(date.year, date.month, day) == 6:  #is the day sunday
+            return day
+    return None 
 
 @app.route('/convert', methods=['POST'])
 def convert():
@@ -20,8 +29,23 @@ def convert():
         "JST":[3,30],
         "IST": [0,0]
     }
+    cur_date = request.form.get('cur_date')
+    cur_datetimeobj = datetime.strptime(cur_date, '%Y-%m-%d')
+    last_sunday_of_month = is_last_sunday(cur_datetimeobj)
 
-    tz = request.form['timezone']
+    #DST Logic
+    if 3<=cur_datetimeobj.month<=10: #within dst months
+        DST=True
+        if cur_datetimeobj.month==3 and cur_datetimeobj.day < last_sunday_of_month:
+            DST=False
+        elif cur_datetimeobj.month==10 and cur_datetimeobj.day > last_sunday_of_month:
+            DST=False
+        else:
+            DST=True
+
+    dec_date = cur_datetimeobj - timedelta(days=1)
+
+    tz = request.form['timezone'] #converting to
     h = int(request.form['hours'])
     m = int(request.form['minutes'])
     ap = request.form['am_pm']
@@ -69,8 +93,14 @@ def convert():
         mins = "0" + mins
 
     result_time = f"{hours}:{mins} {ap}"
-    if(day==0):diff="Behind"
-    else:diff="Ahead of"
-    return render_template('results.html', result_time=result_time,diff=diff,tzn=tzn, tz=tz)
+    dst_time = f"{hours-1}:{mins} {ap}"
+
+    if(day==0):diff=dec_date.date()
+    else:diff=cur_datetimeobj.date()
+    
+    if(DST==True):dst="may apply"
+    else:dst="does not apply" 
+
+    return render_template('results.html', result_time=result_time,diff=diff, tz=tz,dst=dst,dst_time=dst_time)
 if __name__ == '__main__':
     app.run(debug=True)
